9f4be29771d468622388182b72c6e53d3c917da7
GSLeonardo <leo_galindo_01@hotmail.com>
2023-09-12 21:25:30 -0600

    restyle navbar to display profile link at end

--- src/components/drive/DriveNavbar.tsx
+++ src/components/drive/DriveNavbar.tsx
@@ -4,11 +4,11 @@ import { ROUTES } from '../../router';
 
 export default function DriveNavbar() {
 	return (
-		<Navbar bg='light' expand={'sm'} className='p-3'>
+		<Navbar bg='light' expand={true} className='p-3'>
 			<Navbar.Brand as={Link} to={ROUTES.ROOT}>
 				Taco Drive
 			</Navbar.Brand>
-			<Nav>
+			<Nav className='flex-grow-1 justify-content-end'>
 				<Nav.Link as={Link} to={ROUTES.PROFILE}>
 					Profile
 				</Nav.Link>
